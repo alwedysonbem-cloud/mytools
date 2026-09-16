0d223ff339dd1bec37d3367929d840b11d86e733
alwedyson <al.wedyson.bem@impatech.edu.br>
2026-09-16 14:49:55 -0300

    Aula 07 finalizada.p y

--- P07_3490_labirinto.py
+++ P07_3490_labirinto.py
@@ -1,10 +1,6 @@
 import random as rd
 
-def criar_labrinto(l, c, room=' ', wall='W', cheese='*'):
-
-    #lista das direções que posso correr
-    #direct=[(-2, 0), (2, 0), (0, 2), (0, -2)]
-
+def criar_labrinto(l, c, room=' ', wall='O', cheese='*'):
     #crio a matriz com celulas de l linhas e c colunas
     labrinto=[]
     for _ in range(2*l +1):
@@ -46,27 +42,68 @@ def criar_labrinto(l, c, room=' ', wall='W', cheese='*'):
 
     return labrinto
 
-def print_labrintoe(labrintoe):
-    """Imprime o labirinto no terminal, uma linha por vez.
+def busca_em_profundidade(labrnto, lin, col, parede="O", quejo="*"):
+    pila_visit=[(1, 1)] #lista com as coordenadas na ordem de percorrimento
+    visitados={(1, 1)}
+    queijo=()
+    achou=False
+    while pila_visit and not achou: #laço infinito para andar pelo labirinto ate achar o quejo, dai o break
+        vx, vy=pila_visit[-1]
+        #aqui o vx, vy quer dizer o vértice inicial de busca, enquanto que x, y é o vizinho que esta sendo visitado
+        vizinh=[(vx, vy+1), (vx, vy-1), (vx-1, vy), (vx+1, vy)] #as 4 direções para as quais o vertc centro pode ir (em 1 unidade)
+        # rd.shuffle(vizinh)
+        vizi_prox=False
+        for x, y in vizinh:
+            if 0<=x<2*lin+1 and 0<=y<2*col+1 and ((x, y) not in visitados):
+                visitados.add((x, y))
+                if labrnto[x][y]==parede: #achei o fim de um caminho
+                    continue
+                elif labrnto[x][y]==quejo:
+                    achou=True
+                    pila_visit.append((x, y))
+                    vizi_prox=True
+                    queijo=(x, y)
+                    break
+                else:
+                    #primeiro vizinho válido não visitado que está dentro, não é parede nem queijo 
+                    pila_visit.append((x, y))
+                    vizi_prox=True
+                    break
+        if vizi_prox==False:  
+            #isso indica que (x, y) não possui vizinhos válidos para se andar, então vamos para o vertice anterior a ele
+            #com isso, usamos o del para tirá-lo da lista de itens que foram visitados
+            del pila_visit[-1]
+            continue
+    
+    for i, j in pila_visit:
+        if labrnto[i][j]=="*":
+            continue
+        else:
+            labrnto[i][j]="."
 
-    Parameters
-    ----------
-    maze : list[list]
-        Matriz retornada por :func:`generate_maze`.
-    """
+    return labrnto
+
+
+def print_labrintoe(labrintoe):
     for row in labrintoe:
         print(" ".join(map(str, row)))
 
 if __name__ == '__main__':
     m, n = 10, 14  # Grid size
-    rd.seed(10110)
     mazer = criar_labrinto(m, n)
+    print('\nMaze 1')
     print_labrintoe(mazer)
-    print('Maze 1')
+    
 
     room = ' '
-    wall = 'W'
+    wall = 'O'
     cheese = '*'
     maze = criar_labrinto(m, n, room, wall, cheese)
-    print_labrintoe(maze)
+
     print('\nMaze 2')
+    print_labrintoe(maze)
+    casa=busca_em_profundidade(maze, m, n)
+    print()
+    print('Maze com o caminho até o queijo')
+    print_labrintoe(casa)
+    
